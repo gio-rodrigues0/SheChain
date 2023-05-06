57039a6e1999513b6191cc5c68f71d85f60ade57
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2023-05-06 09:24:42 -0300

    create signup page

--- diana/src/index.js
+++ diana/src/index.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import LoginPage from "./views/LoginPage/LoginPage";
 import LandingPage from "./views/LandingPage/LandingPage";
+import SignUpPage from "./views/SignUpPage/SignUpPage";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/signup" element={<SignUpPage />}/>
     </Routes>
   </Router>,
   document.getElementById("root")
