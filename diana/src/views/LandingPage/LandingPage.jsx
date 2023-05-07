6f4e9397109bfc5f284a0ee3fdc043afc8629f93
SOPHIA <sophia.tosar@sou.inteli.edu.br>
2023-05-07 08:54:40 -0300

    landing page

--- diana/src/views/LandingPage/LandingPage.jsx
+++ diana/src/views/LandingPage/LandingPage.jsx
@@ -19,20 +19,21 @@ function LandingPage() {
         <Image src={logo}></Image>
       </div>
 
+    <div>
       <Button className="entrar">Entrar</Button>
-
-      <Button className="cadastrar">Cadastrar</Button>
-
+      <Button ghost className="cadastrar">Cadastrar</Button>
+      </div>
     </div>
-
+<div className="escrita">
     <div className="main">
-      <h1>diana</h1>
+      <p>diana</p>
     </div>
 
     <div className="slogan">
-      <p>Juntos pela <p className="verde">sustentabilidade</p>,impulsionados pelo <p className="verde">blockchain.</p></p>
+      <p>Juntos pela sustentabilidade</p>
+      <p>Impulsionados pelo blockchain</p>
     </div>
-
+    </div>
     <div className="landing-page-footer">
     </div>
     </>
